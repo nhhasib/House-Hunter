3c621ebb0cf49b3fd4261416a090c70cbada7cec
nhhasib <nafiul.hasan1997@gmail.com>
2024-01-24 00:50:43 +0600

    home page added

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import logo from "../assets/images/logo.png"
+
+const NavBar = () => {
+    return (
+      <div>
+        <div className="w-11/12 mx-auto">
+          <div className="navbar bg-base-100 flex justify-between">
+            <div className="">
+              <a className="">
+                <img className="w-48" src={logo} alt="" srcset="" />
+              </a>
+            </div>
+            <div className="flex justify-between w-1/2">
+              <div className="form-control w-4/5">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="input input-bordered w-24 md:w-auto"
+                />
+              </div>
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS Navbar component"
+                      src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    />
+                  </div>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <a className="justify-between">
+                      Profile
+                      <span className="badge">New</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>Settings</a>
+                  </li>
+                  <li>
+                    <a>Logout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=''>
+          <div className="navbar bg-blue-400">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <a>Item 1</a>
+                  </li>
+                  <li>
+                    <a>Parent</a>
+                    <ul className="p-2">
+                      <li>
+                        <a>Submenu 1</a>
+                      </li>
+                      <li>
+                        <a>Submenu 2</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>Item 3</a>
+                  </li>
+                </ul>
+              </div>
+              
+            </div>
+            <div className="navbar-center hidden lg:flex text-[50px] text-white font-bold">
+              <ul className="menu menu-horizontal px-1 text-xl">
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>Profile</a>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div>
+            <div className="navbar-end">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default NavBar;
--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import NavBar from '../component/NavBar';
+import bg1 from "../assets/images/bg-1.jpg"
 
 const HomeLayout = () => {
     return (
-        <div>
-            <h1>from home layout
-                <button className='btn btn-primary'>akhsdfkj</button>
-            </h1>
+      <div
+        className="h-svh"
+        style={{
+          backgroundImage: `url(${bg1})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className='bg-black bg-opacity-50 py-4 my-auto'>
+          <h1 className="text-2xl text-white text-center font-bold">
+            "Unlocking Your Dream Home: Your Key to a Better Future with
+            HouseHunter!"
+          </h1>
         </div>
+      </div>
     );
 };
 
